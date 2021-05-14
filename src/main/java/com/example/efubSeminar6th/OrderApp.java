c50e225402dcc6f0c55181ba4e9a506faf94647e
시연 김 <ashleysmile3@gmail.com>
2021-05-14 21:40:44 +0900

    ok

---
+++ src/main/java/com/example/efubSeminar6th/OrderApp.java
@@ -0,0 +1,34 @@
+package com.example.efubSeminar6th;
+
+import com.example.efubSeminar6th.member.Grade;
+import com.example.efubSeminar6th.member.Member;
+import com.example.efubSeminar6th.member.MemberService;
+import com.example.efubSeminar6th.member.MemberServiceImpl;
+import com.example.efubSeminar6th.order.Order;
+import com.example.efubSeminar6th.order.OrderService;
+import com.example.efubSeminar6th.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OrderApp {
+    public static void main(String[] args) {
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext =  new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+        long memberId = 1L;
+
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
