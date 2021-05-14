82ebba5f6622e347a1618b1c6f7a3dcde6305137
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2021-05-14 21:49:53 +0900

    Add files via upload

---
+++ 6주차/efubSeminar6th/src/main/java/com/example/efubSeminar6th/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.efubSeminar6th.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
